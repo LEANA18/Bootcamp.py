7604590eba17b2616d3b9f9f4aaf67966ddd707d
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-02-11 23:18:23 +0530

    Update StringModification.py

--- StringModification.py
+++ StringModification.py
@@ -1,3 +1,9 @@
+"""
+Write a python program to accept a string as input. 
+Create a new string by removing the first two characters from the input string and converting all the alphabets to smaller case. 
+Print the new string as output.
+"""
+
 str=str(input())
 newstr=str[2:]
 print(newstr.lower())
