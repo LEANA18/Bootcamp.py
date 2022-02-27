e8b687d8c0d03aef4a181fbcbd51246f37ed659e
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-02-27 22:15:25 +0530

    Create Differentangle.py

---
+++ Differentangle.py
@@ -0,0 +1,11 @@
+"""
+Zyphernova is in big trouble. It is waging war against Planet Deltonia. The army of Zyphernova is running out of weapons!!...
+
+it must aquire new weapon from another planet called Hypertron. But there is a problem. Hypertron uses Radian unit to measure angles. Hence we need to convert it
+
+write a program that accepts a string of radian measures and outputs the corresponding degree measures
+"""
+
+import math
+a = float(input())
+print(math.degrees(a))
